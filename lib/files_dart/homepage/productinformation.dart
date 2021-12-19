c331935a789998f722c6c70b4f49caff033d9c63
Badsha <mdarifhasan2004@gmail.com>
2021-12-19 13:02:41 +0600

    20-12-2021

---
+++ lib/files_dart/homepage/productinformation.dart
@@ -0,0 +1,69 @@
+import 'package:bango_shop/files_dart/product_details/custom_product_display.dart';
+class productinformation {
+  final List<custom_product_display> prouctname = [
+    custom_product_display(
+        "Men's Strysish Shoes",
+        "https://i.pinimg.com/originals/f4/5f/60/f45f608cc8624fd6404b0aa6df42607f.png",
+        5,
+        1000,
+        0.1,
+        "50%",
+        1200),
+    custom_product_display(
+        "Girl's stylish shirt",
+        "https://www.pngkit.com/png/full/224-2245139_space-kitteh-girls-t-shirt-kawaii-kid-shirts.png",
+        4.6,
+        220,
+        0.1,
+        "80%",
+        340),
+    custom_product_display(
+        "M120 Smart band watch",
+        "https://www.pngarts.com/files/9/Android-Smart-Band-PNG-Download-Image.png",
+        5,
+        1200,
+        0.1,
+        "30%",
+        1500),
+    custom_product_display(
+        "Latest white lighter",
+        "https://i.pinimg.com/originals/25/ea/12/25ea121fd6c3ec7f7fa31b8f859368ae.png",
+        5,
+        600,
+        0.1,
+        "40%",
+        900),
+    custom_product_display(
+        "I-phone 13 pro max cover",
+        "https://www.pngall.com/wp-content/uploads/11/iPhone-13-Pro-Max.png",
+        5,
+        240,
+        0.1,
+        "22%",
+        400),
+    custom_product_display(
+        "new dj cap 2022",
+        "https://i.pinimg.com/originals/8c/7d/26/8c7d2654ba4dfeb153063cada60486b8.png",
+        5,
+        00,
+        0.1,
+        "100%",
+        1500),
+    custom_product_display(
+        "Latest Mi Headphone x3",
+        "https://atlas-content-cdn.pixelsquid.com/stock-images/playstation-5-headphones-724R2n7-600.jpg",
+        5,
+        1990,
+        0.1,
+        "20%",
+        2300),
+    custom_product_display(
+        "Stylish Orenge Shirt",
+        "https://purepng.com/public/uploads/large/purepng.com-t-shirt-orangefashiondressshirtclothtshirtclothingt-shirt-orange-631522326912s0fqp.png",
+        5,
+        786,
+        0.1,
+        "30%",
+        1000),
+  ];
+}
